73d31c3cb55b6c98b2bc7f60b31a2dcc1332a891
vitornuness <vitor1nuness@gmail.com>
2025-05-01 07:53:58 -0300

    feat: configuracao do provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
+import 'package:chamada/ui/viewmodel/aluno_view_model.dart';
+import 'package:chamada/ui/viewmodel/auth_view_model.dart';
+import 'package:chamada/ui/viewmodel/justificativa_view_model.dart';
+import 'package:chamada/ui/viewmodel/registro_view_model.dart';
+import 'package:chamada/ui/viewmodel/reserva_view_model.dart';
+import 'package:chamada/ui/viewmodel/sala_view_model.dart';
+import 'package:chamada/ui/viewmodel/turma_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:chamada/data/service/auth_service.dart';
 import 'package:chamada/router.dart';
 
 void main() {
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthService())],
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthViewModel()),
+        ChangeNotifierProvider(create: (context) => AlunoViewModel()),
+        ChangeNotifierProvider(create: (context) => JustificativaViewModel()),
+        ChangeNotifierProvider(create: (context) => RegistroViewModel()),
+        ChangeNotifierProvider(create: (context) => ReservaViewModel()),
+        ChangeNotifierProvider(create: (context) => SalaViewModel()),
+        ChangeNotifierProvider(create: (context) => TurmaViewModel()),
+      ],
       child: MainApp(),
     ),
   );
